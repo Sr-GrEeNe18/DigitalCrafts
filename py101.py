8d3a4996ae7cb3c5e439f571bca1435b1fdff0b3
Sr-GrEeNe <81869226+Sr-GrEeNe18@users.noreply.github.com>
2021-04-06 23:40:41 -0400

    Add files via upload

--- py101.py
+++ py101.py
@@ -1,5 +1,37 @@
-print("hello world")
+# print("hello world")
 
-# isinstance(5,int)
+# # isinstance(5,int)
 
-# variables type: int, str, object 
+# print ("hello part deux")
+
+# print ("abc")
+
+# print ("cde")
+
+# print ("Rio" + " " + "Greene")
+
+# print ('This is a great day!')
+
+# print('\tThis is a\n great day/')
+
+
+
+
+# # 
+# # variables type: int, str, object 
+# print (5 +4)
+
+# print (5 ** 2)
+# print (5 /2)
+# print (5 % 2)
+
+# city = "Killeen"
+# print (city)
+
+# 
+first_name = "Rio"
+result = f'Hello {first_name}'
+print(result)
+
+
+
