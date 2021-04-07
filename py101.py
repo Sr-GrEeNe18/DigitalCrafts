f15917820159dc4bdd2fd503bb4048b128cf62e9
Sr-GrEeNe <81869226+Sr-GrEeNe18@users.noreply.github.com>
2021-04-07 11:00:40 -0400

    Add files via upload

--- py101.py
+++ py101.py
@@ -29,9 +29,27 @@
 # print (city)
 
 # 
-first_name = "Rio"
-result = f'Hello {first_name}'
-print(result)
+# first_name = "Rio"
+# result = f'Hello {first_name}'
+# print(result)
 
+# print(f'Your name is {name}')
+
+# age = int(input("How old are you >>"))
+# ageInt = int (age)
+
+# print(type(ageInt))
+
+# if age >=21:
+
+#     print("you get a free beer")
+
+# print("outside of our condition")
+
+
+count = 0 
+
+while count < 10:
+    print(f'The count is {count}')
 
 
