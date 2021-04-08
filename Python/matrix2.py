36acdcb9f0ab4e7b4cb6b321e70c4c92236e159b
Rio <pla_honey1@yahoo.com>
2021-04-08 17:44:40 -0400

    commit in homework2

---
+++ Python/matrix2.py
@@ -0,0 +1,9 @@
+matrixone = [[1, 3], [2, 4]]
+matrixtwo = [[]]
+matrixsum = [], []
+
+inputArray = []
+
+for indexone in range(0, len(matrixone), 1):
+    for indextwo in range(0, 2, 1):
+        print(f'indexone: {indexone}, indextwo: {indextwo}')
