89b9668773b6bbec9e21c93f75cbfe000f5b47e1
Rio <pla_honey1@yahoo.com>
2021-04-08 13:03:53 -0400

    commit on homework2

---
+++ Python/loops.py
@@ -0,0 +1,35 @@
+
+# 
+# 
+# listA = [1, 2, 3, 4, 5]
+
+# listB = listA
+
+# listB[1] = 99
+
+# print(listA)
+# print(listB)
+
+# alphabet = "tltltlltltlltlltlltl"
+
+# index = 0
+# while index <= len(alphabet) - 1:
+#     print(alphabet[index])
+#     index += 1 
+
+# constants = (3.14, 2.71)
+
+# 
+
+##)
+# name = "syreeta greene"
+
+# for gifted in name:
+#     print(gifted)
+#     # for loops
+
+#largest number 
+arr = [4, 2, 5, 7, 23, 6, 5]
+
+# 
+
